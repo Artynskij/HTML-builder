851485d842d1f053fe0eece54120baf66f5e53ed
artem <aartynskij0001@gmail.com>
2024-01-17 14:40:20 +0300

    feat: add 2 part and refactor 1 part

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
-// const path = require('path');
-fs.readFile('./01-read-file/text.txt', 'utf8', (err, data) => {
+const path = require('path');
+const pathFile = path.format({
+  root: '/',
+  base: 'text.txt',
+  ext: 'ignored',
+});
+const url = __dirname + pathFile;
+
+fs.readFile(url, 'utf8', (err, data) => {
   if (err) throw err;
   console.log(data);
 });
---
+++ 02-write-file/index.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+const pathFile = path.format({
+  root: '/',
+  base: 'hello.txt',
+  ext: 'ignored',
+});
+const url = __dirname + pathFile;
+const writeableStream = fs.createWriteStream(url);
+
+let readline = require('readline');
+let readlineInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '>',
+});
+// readlineInterface.prompt();
+let guestName = '';
+readlineInterface.question('What is your name? ', (answer) => {
+  guestName = answer;
+  console.log(`Hello ${answer}`);
+  console.log('You can write');
+  readlineInterface.prompt('>');
+});
+// readlineInterface.on('history', (input) => {
+//   console.log(input);
+//   if (!input.find((item) => item === 'exit')) {
+//     readlineInterface.prompt('>');
+//   }
+// });
+readlineInterface.on('line', (input) => {
+  if (input.toLowerCase() === 'exit') {
+    readlineInterface.close();
+    return;
+  }
+  writeableStream.write(`${input}\n`);
+  console.log(input);
+  readlineInterface.prompt('>');
+});
+
+readlineInterface.on('close', () => {
+  console.log(`good night ${guestName}`);
+});
