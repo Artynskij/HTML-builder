30c8ac24a42a09422a5486d9b3849c8860517b90
artem <aartynskij0001@gmail.com>
2024-01-24 08:29:01 +0300

    fix:part 1. add readStream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,7 +3,13 @@ const path = require('path');
 
 const url = path.join(__dirname, 'text.txt');
 
-fs.readFile(url, 'utf8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
+// fs.readFile(url, 'utf8', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+const readStream = fs.createReadStream(url, 'utf8');
+
+readStream.on('data', (chunk) => {
+  console.log(chunk);
 });
